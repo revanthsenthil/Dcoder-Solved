4d8c84d6e215a10b90136edfd3795d55760c7cbc
rev0 <30791201+revanthsenthil@users.noreply.github.com>
2020-08-11 18:12:35 +0530

    Create PrintHelloWorld.java
    uploaded first solution

---
+++ PrintHelloWorld.java
@@ -0,0 +1,28 @@
+"""
+Problem Description:
+Alice recently discovered programming.Help Alice to code her first Program.
+
+Input:
+
+Output:
+Hello World
+
+Constraints:
+
+Sample Input:
+
+Sample Output:
+Hello World
+"""
+
+// this is the first code that a programmer writes while starting a language
+
+import java.util.*;
+
+class Dcoder
+{  
+  public static void main(String args[])
+  {  
+    System.out.println("Hello World");
+  }
+}
