00b6ddbf360ae022b859f02dea86d524f9c36dc2
rev0 <30791201+revanthsenthil@users.noreply.github.com>
2020-08-11 14:59:35 +0530

    Create PrintHelloWorld.c
    uploaded solved solution

---
+++ PrintHelloWorld.c
@@ -0,0 +1,26 @@
+/*
+Problem Description:
+Alice recently discovered programming.Help Alice to code her first Program.
+
+Input:
+
+Output:
+Hello World
+
+Constraints:
+
+Sample Input:
+
+Sample Output:
+Hello World
+*/
+
+//This is the most basic and first step into programming in any language
+
+#include  <stdio.h>
+
+int main()
+{
+  printf("Hello World");
+
+}
