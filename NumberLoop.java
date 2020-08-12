917558ad30e0915638a3a8fc051234fd444f5d3f
rev0 <30791201+revanthsenthil@users.noreply.github.com>
2020-08-12 12:09:19 +0530

    Create NumberLoop.java
    uploaded solved solution

---
+++ NumberLoop.java
@@ -0,0 +1,35 @@
+/*
+Problem Description:
+Ariel & Eric have a child, help them teaching him Counting from 1 to 10.
+
+Input:
+
+Output:
+Output should be 1 to 10 separated with a new line character "\n"
+
+Constraints:
+
+Sample Input:
+
+Sample Output:
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+*/
+
+class NumberLoop
+{  
+  public static void main(String args[])
+  {  
+    for(int i = 1; i<= 10; i++){
+        System.out.println(i);
+    }
+  }
+}
