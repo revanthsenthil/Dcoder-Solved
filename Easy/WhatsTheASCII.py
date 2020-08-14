235632fd2664096083806e01c23271d4a9c1347c
rev0 <30791201+revanthsenthil@users.noreply.github.com>
2020-08-15 01:03:38 +0530

    Create WhatsTheASCII.py
    uploaded solved solution

---
+++ Easy/WhatsTheASCII.py
@@ -0,0 +1,22 @@
+"""
+Problem Description:
+You will be given a character having ASCII Code between 33 to 127. You need to print the ASCII Code of the character.
+
+Input:
+A single character.
+
+Output:
+Print the ASCII Code of the character in decimal form.
+
+Constraints:
+Character will have ASCII code between 33 and 127.
+
+Sample Input:
+A
+
+Sample Output:
+65
+"""
+
+n = input()
+print(ord(n))
