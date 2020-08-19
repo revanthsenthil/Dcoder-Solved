10d38d2c6a22ce2f38bb75fd6e0406dd78a749e7
rev0 <30791201+revanthsenthil@users.noreply.github.com>
2020-08-19 07:11:23 +0530

    Create Title.py
    UPLOADED solved solution

---
+++ Easy/Title.py
@@ -0,0 +1,22 @@
+"""
+Problem Description:
+You will be given a string, all you have to do is capitalize first letter of each word in the given string.
+
+Input:
+Input contains a string s.
+
+Output:
+Print the manipulated string according to the problem described.
+
+Constraints:
+1≤s length≤1000
+
+Sample Input:
+i love Dcoder
+
+Sample Output:
+I Love Dcoder
+"""
+
+n = input()
+print(n.title())
