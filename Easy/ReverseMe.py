682981ca813b5c9a962dca2d6bf1bb8043d8d273
rev0 <30791201+revanthsenthil@users.noreply.github.com>
2020-08-19 16:16:29 +0530

    Create ReverseMe.py
    UPLOADED solved solution

---
+++ Easy/ReverseMe.py
@@ -0,0 +1,22 @@
+"""
+Problem Description:
+You are given an integer, reverse it.
+
+Input:
+Input contains an integer n.
+
+Output:
+Print reverse of integer.
+
+Constraints:
+1≤n≤10000
+
+Sample Input:
+9956
+
+Sample Output:
+6599
+"""
+
+n = input()[::-1]
+print(int(n))
