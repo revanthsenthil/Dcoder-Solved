7d23d4d3eff8ed7455170da0a2fea72be06f31fd
Revanth S <30791201+revanthsenthil@users.noreply.github.com>
2020-10-16 11:09:38 +0530

    Update CodysAssignment.py

--- Hard/CodysAssignment.py
+++ Hard/CodysAssignment.py
@@ -5,5 +5,5 @@ s = input()
 
 for i in range(n):
   a, b = [int(j) for j in input().split()]
-  g = [ord(j) for j in s[a - 1:b]]
-  print(chr(min(g)))
+  g = [ord(j) for j in s[a - 1:b]]        # this list contains the ASCII values of individual characters in the range from the input
+  print(chr(min(g)))                      # the problem asked for minimum lexicological value
